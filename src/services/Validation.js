2a6f8ee572cd4f4c9b456f6c859ccd64e723d8aa
Piranja <techpiranja@gmail.com>
2020-10-24 16:57:43 +0200

    / removed yarn test

--- src/services/Validation.js
+++ src/services/Validation.js
@@ -1,8 +1,8 @@
 class Validation {
-	validURL(str) {
-		var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-		return regexp.test(str);
-	}
+  validURL(str) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(str);
+  }
 }
 
 const Validator = new Validation();
